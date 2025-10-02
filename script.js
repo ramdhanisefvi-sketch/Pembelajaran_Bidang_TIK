58dedd75c6ecd08baf8b63893ea546a772252ef6
ramdhanisefvi-sketch <ramdhanisefvi@gmail.com>
2025-10-02 12:12:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+conts tombolSapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click'.funcation(){
+  alert('Hallo! Terimakasih sudah berkunjung!')
+}):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
